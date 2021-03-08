b6e6e43b5a319d228e5df06cac937d6715fa6f2b
gskam <gskam@LAPTOP-DV0ACVNH>
2021-03-08 18:45:15 +0530

    commit

---
+++ E__WALLET__APP/src/mybeans/DBConnector.java
@@ -0,0 +1,24 @@
+package mybeans;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnector 
+{
+	public Connection getConnected()
+	{
+		Connection con=null;
+				
+		try
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/e_wallet ? user=root&password=garry74");
+		}
+		catch(Exception e)
+		{
+			
+		}
+		return con;
+	}
+
+}
